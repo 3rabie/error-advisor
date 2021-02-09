c7edd63849ca6551e923f2cbf812c35499fdca80
Abdulrahman Rabie <abdulrahman.rabie@ibm.com>
2021-02-09 03:31:27 +0200

    fix options issue

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import Utils from "./advisor/util/Utils";
 import { logger } from "./logger";
 
-export default function errorAdvisor(debug: Boolean, log: Boolean, err: any, req: any, res: any) {
+export default function errorAdvisor(options: any, err: any, res: any) {
 
     let error = Utils.errorBuilder(err.message, err.unauthorized, err.status, err.timestamp);
 
-    if (debug) {
+    if (options.debug) {
         error.stack = err.stack;
     }
 
-    if (log && typeof log === 'boolean') {
+    if (options.log && typeof options.log === 'boolean') {
         logger.error(error);
     }
 
